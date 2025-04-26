ad65d9a17bd8b59bf8d17d86adee9c34e14dfd65
Alejo <istominalexey@ro.ru>
2025-04-26 09:35:40 +0300

    task fixed

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,4 +1,6 @@
 const t = document.getElementById('timer')
 setInterval(() => {
     t.textContent = Number(t.textContent) - 1
+    if (Number(t.textContent) === 0) {
+        alert('Вы победили в конкурсе')}
 }, 1000)
